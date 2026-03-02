4d0d0c512d8ab7e3e4d87b198b61a37112fc6f1c
junSung <not971006@gmail.com>
2026-03-03 00:25:21 +0900

    Fix/UI 정렬 수정 및 접근성 강화

--- front-end/src/feature/auth/ui/LoginFrom.tsx
+++ front-end/src/feature/auth/ui/LoginFrom.tsx
@@ -1,16 +1,16 @@
-import { Input } from "@/shared/ui/atoms/input";
-import { Button } from "@/shared/ui/atoms/button";
+import { Input } from "@/shared/ui/atoms/Input";
+import { Button } from "@/shared/ui/atoms/Button";
 
 const LoginForm = () => {
     return (
         <>
-        <div className="flex flex-col gap-8">
+          <form className="flex flex-col gap-8">
             <div className="flex flex-col gap-4">
               <Input placeholder="이메일을 입력해주세요."/>
               <Input placeholder="비밀번호를 입력해주세요." type="password" />
             </div>
             <Button>로그인</Button>
-          </div>
+          </form>
         </>
     );
 };
