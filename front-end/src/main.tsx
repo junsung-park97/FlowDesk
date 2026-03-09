8552496546fb5894a5d12d00cee06ce2de973fd1
junSung <not971006@gmail.com>
2026-03-09 14:46:53 +0900

    task/브라우저 서비스 워커 활용하여 api 요청,응답 테스트

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -1,10 +1,23 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 import "./style/index.css";
-import App from "./App.tsx";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+async function enableMocking() {
+  if (!import.meta.env.DEV) {
+    return;
+  }
+  const { worker } = await import("./shared/api/mock/browser");
+  return worker.start();
+}
+
+enableMocking().then(() => {
+  ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>,
+  );
+});
