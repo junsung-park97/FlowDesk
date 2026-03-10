98d050ce18a2641d6e492116b21722ddbbf69993
GolemOnce <shh3350@gmail.com>
2026-03-10 21:58:00 +0900

    Feat/개발환경 세팅 및 프로젝트 구조 초기화 v5

--- back-end/src/main/java/flowDesk/FlowDeskApplication.java
+++ back-end/src/main/java/flowDesk/FlowDeskApplication.java
@@ -1,4 +1,4 @@
-package FlowDesk;
+package flowDesk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- back-end/src/test/java/flowDesk/FlowDeskApplicationTests.java
+++ back-end/src/test/java/flowDesk/FlowDeskApplicationTests.java
@@ -1,4 +1,4 @@
-package FlowDesk.FlowDesk;
+package flowDesk;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
